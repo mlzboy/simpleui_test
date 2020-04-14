dc0a8b907477392989856809de6fabd2f02cc2eb
mlzboy <frederick.mao@gmail.com>
2020-04-14 10:53:30 +0800

    zz

--- syncdb.bat
+++ syncdb.bat
@@ -5,6 +5,6 @@ rmdir /S /Q oj\__pycache__
 rmdir /S /Q online_judge\__pycache__
 python manage.py makemigrations oj
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin888', 'admin888@example.com', 'admin888')"
 python manage.py runscript load_data
 python manage.py runserver
