b3be2113db8d723400e93925dad5a6d7a3b47cc0
mlzboy <frederick.mao@gmail.com>
2020-11-21 08:00:55 +0800

    为生产环境使用bitnamistack django做兼容

--- simpleui_test/wsgi.py
+++ simpleui_test/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
-
+import os,sys
+sys.path.append('C:/Users/mlzboy/Bitnami Django Stack projects/simpleui_test')
+#sys.path.append('C:/Users/mlzboy/Bitnami Django Stack projects/simpleui_test/simpleui_test')
+os.environ.setdefault("PYTHON_EGG_CACHE", "C:/Users/mlzboy/Bitnami Django Stack projects/simpleui_test/egg_cache")
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simpleui_test.settings')
