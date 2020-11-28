0452e51e9979eff262cbebe811aeb0aae4a9e253
JohnserfSeed <2080979987@qq.com>
2020-11-28 15:38:03 +0800

    Revert "Merge pull request #2 from mlzboy/vue"
    This reverts commit 51ac9085d90f7e4484aa6955ec13d38ff7d6c906, reversing
    changes made to 94294454c5445812b6545ee8f8634648087510df.

--- simpleui_test/settings.py
+++ simpleui_test/settings.py
@@ -88,7 +88,7 @@ ROOT_URLCONF = 'simpleui_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['vue/dist'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -168,7 +168,7 @@ print(STATIC_ROOT)
 print(BASE_DIR)
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'vue/dist/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # 自定义simpleui 菜单
